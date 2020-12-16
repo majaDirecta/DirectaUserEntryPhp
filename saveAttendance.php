982059062ffb50993b1196a2aeff97be216aed1d
Matija Zorkovic <matija.zorkovic@hiwe-it.com>
2020-12-16 13:48:43 +0100

    #2435 n4

--- saveAttendance.php
+++ saveAttendance.php
@@ -4,8 +4,8 @@
     $post = file_get_contents('php://input');
     $json = json_decode($post, true);
  
-	$sql = "INSERT INTO checkinout (licenceId, Userid, source_user,CheckTime,source_time,CheckType,source_type,data_source, Sensorid)
-		VALUES ('".$json['licenceId']."', '".$json['Userid']."', '".$json['source_user']."','".$json['CheckTime']."','".$json['source_time']."','".$json['CheckType']."','".$json['source_type']."','".$json['data_source']."','".$json['Sensorid']."')";
+	$sql = "INSERT INTO checkinout (licenceId, Userid, source_user,CheckTime,source_time,CheckType,source_type,data_source, GEOIP, Sensorid)
+		VALUES ('".$json['licenceId']."', '".$json['Userid']."', '".$json['source_user']."','".$json['CheckTime']."','".$json['source_time']."','".$json['CheckType']."','".$json['source_type']."','".$json['data_source']."','".$json['GEOIP']."','".$json['Sensorid']."')";
 
 	if ($conn->query($sql) === false) {
 		exit('{"status":500, "result":"Error: ' . $sql . '<br>' . $conn->error . '"');
--- saveFailedAttendance.php
+++ saveFailedAttendance.php
@@ -6,8 +6,8 @@
   
 	$array = array();
     foreach ($json as $record) {
-        $sql = "INSERT INTO checkinout (licenceId, Userid, source_user,CheckTime,source_time,CheckType,source_type,data_source, Sensorid)
-        VALUES ('".$record['licenceId']."', '".$record['Userid']."', '".$record['source_user']."','".$record['CheckTime']."','".$record['source_time']."','".$record['CheckType']."','".$record['source_type']."','".$record['data_source']."','".$record['Sensorid']."')";
+        $sql = "INSERT INTO checkinout (licenceId, Userid, source_user,CheckTime,source_time,CheckType,source_type,data_source, GEOIP, Sensorid)
+        VALUES ('".$record['licenceId']."', '".$record['Userid']."', '".$record['source_user']."','".$record['CheckTime']."','".$record['source_time']."','".$record['CheckType']."','".$record['source_type']."','".$record['data_source']."','".$json['GEOIP']."','".$record['Sensorid']."')";
         if ($conn->query($sql) === true) {
 			array_push ( $array , $record['Logid'] );
         } 
